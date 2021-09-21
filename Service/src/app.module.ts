96a839b6a9549358c75c8591adec29a54c1d734e
Laughing6901 <vandat96tn@gmail.com>
2021-09-22 03:27:54 +0700

    add docker-compose

--- Service/src/app.module.ts
+++ Service/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ValidationPipe } from './config/validation.pipe';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide:APP_PIPE,
+    useClass:ValidationPipe,
+  },
+  AppService],
   exports:[]
 })
 export class AppModule {}
--- Service/src/users/create-user.dto.ts
+++ Service/src/users/create-user.dto.ts
@@ -1,7 +1,16 @@
+import { IsInt, IsString } from "class-validator";
+
 export class CreateUserDto  {
+    @IsString()
     name: string;
+
+    @IsInt()
     age: number;
+
+    @IsString()
     address: string;
+
+    @IsString()
     job:string;
 
 }
