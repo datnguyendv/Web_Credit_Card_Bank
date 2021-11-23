919ca763e7d6ed6ee56ecea21c831f52a59c4a6d
Laughing6901 <vandat96tn@gmail.com>
2021-11-23 21:23:44 +0700

    return version

--- Service/src/modules/auth/modules/signUp.ts
+++ Service/src/modules/auth/modules/signUp.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AccountRegisterDto, findUserDto } from "src/modules/account/dto/account.dto";
 import { User } from "src/modules/account/entities/account.entity";
 import { AccountService } from "src/modules/account/services/account.service";
@@ -22,14 +22,10 @@ export class SignUp {
             throw new UnauthorizedException("User existed");
         } else {
             //create account 
-            try {
-                let accountCreated:User = await this.accountService.createAccount(account);
-                //return jwt with role = User because just for user register account
-                return this.signUser.signUser(accountCreated.AccountId, accountCreated.UserName, "User");
-            } catch (error) {
-                throw new BadRequestException('please register again');
-            }
+            let accountCreated:User = await this.accountService.createAccount(account);
             
+            console.log("SignUp(): ", accountCreated);
+            return this.signUser.signUser(accountCreated.IdentifyCard, accountCreated.UserName, "User");
         }
     }
 }
