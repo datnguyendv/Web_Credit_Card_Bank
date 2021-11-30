53d2d3d94073b8cb69f15ae830eaafe6670a2566
Laughing6901 <vandat96tn@gmail.com>
2021-11-30 08:49:25 +0700

    done send mail modified html

--- Service/src/modules/auth/email/sendmail.ts
+++ Service/src/modules/auth/email/sendmail.ts
@@ -16,16 +16,31 @@ export class SendMail {
         let transporter = nodemailer.createTransport({
             service:'gmail',
             auth: {
-              user: EMAIL_USERNAME, // generated ethereal user
-              pass: EMAIL_PASSWORD, // generated ethereal password
+              user: EMAIL_USERNAME, // username of gmail
+              pass: EMAIL_PASSWORD, // password of this gmail
             },
         });
         let mailOption = {
             from: 'setopic14banking@gmail.com', // sender address
             to: `${email}`, // list of receivers
-            subject: "[BANK NAME]", // Subject line
-            html: `<p>Ma OTP cua ban la <b>${otp}</b>. OTP hieu luc trong 90 giay. Tran trong!</p>
-                   <p>Your OTP code is <b>${otp}</b>. Just need in 90 seconds. Thankyou!</p>`
+            subject: "[IUB Digitalbank] Mã Xác Thực Giao Dịch / Transaction Verification Code", // Subject line
+            html: `<p style="font-size:16px;">Mã xác thực là <b>${otp}</b>, có hiệu lực trong vòng 1 phút. Chi tiết giao dịch: Chuyển khoản bằng thẻ trên  website IUB Digibank.</p>
+
+            <p style="font-size:16px;">Đây là email tự động. Quý khách vui lòng không gửi thư vào địa chỉ này. Mọi vướng mắc liên quan đến dịch vụ, Quý khách vui lòng tham khảo tại website InternationalUniversityBank https://portal.iubank.com.vn hoặc liên hệ với Hotline 24/7 theo số 1900545454.</p>
+
+            <p style="font-size:16px;"><b>Cám ơn Quý khách đã sử dụng dịch vụ của InternationalUniversityBank!</b></p>
+
+            <hr>
+
+            <p style="font-size:16px;">Your OTP is <b>${otp}</b>, valid in 1 minutes.Transaction details: Transfer via card on Website of IUB Digibank.</p>
+            
+            <p style="font-size:16px;">Please do not reply to this automatically-generated email. For further information, please direct your inquiries to: <br>
+            - Our website: https://portal.iubank.com.vn <br>
+            - Our Hotline 24/7 at 1900 54 54 54</p>
+
+            <p style="font-size:16px;"><b>Thank you for banking with InternationalUniversityBank!</b></p>
+            `
+            
         }
         let info = await transporter.sendMail(mailOption, (err,data) => {
             if(err) {
