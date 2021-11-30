63ac39bf4346423752f85daff4ce61ed95146d38
Laughing6901 <vandat96tn@gmail.com>
2021-11-30 07:44:38 +0700

    done get all login his

--- Service/src/modules/loginHis/controllers/login.controller.ts
+++ Service/src/modules/loginHis/controllers/login.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginService } from '../services/login.service';
 
-@Controller('login')
-export class LoginController {}
+@Controller('loginHis')
+@UsePipes(new ValidationPipe())
+export class LoginController {
+    constructor(
+        private loginService: LoginService,
+    ){}
+
+    @Get()
+    async getAllLoginHis(): Promise<any> {
+        return this.loginService.getAllLoginHis();
+    }
+}
