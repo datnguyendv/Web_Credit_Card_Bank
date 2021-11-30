ff6af035ae4ef57da007fb5f7331e84b4b9db598
Laughing6901 <vandat96tn@gmail.com>
2021-12-01 05:07:27 +0700

    modify

--- Service/src/modules/account/modules/manage-account.ts
+++ Service/src/modules/account/modules/manage-account.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import * as bcrypt from 'bcryptjs';
 import { AccountRegisterDto, updatePassword } from '../dto/account.dto';
 import { UserRepository } from "../repositories/account.repository";
@@ -11,8 +11,10 @@ export class ManageAccount {
 
     async saveAccount(account: AccountRegisterDto): Promise<any> {
         let result = await this.userRepository.save(account);
-        console.log(result);
-        return true;
+        if (result !== undefined) {
+            return true;
+        } else  
+            return false;
     }
 
     async createAccount(account: AccountRegisterDto):Promise<any> { 
@@ -20,17 +22,23 @@ export class ManageAccount {
         let hashPassword = await bcrypt.hash(account.Password, salt);
         account.Password = hashPassword;
         console.log("createAccount(): ", account);
-        await this.saveAccount(account)
-        return account;
+        let result = await this.saveAccount(account);
+        if(result === true) {
+            return account;
+        } else 
+        throw new BadRequestException('wrong infomation');
+        
     }
 
     async updateAccountPass(params: updatePassword):Promise<boolean> {
         let salt = await bcrypt.genSalt(10)
         let hashPassword = await bcrypt.hash(params.Password, salt);
         console.log(params);
-        let result = await this.userRepository.update(params.ID, {Password: hashPassword});
-        console.log(result);
-        return true;
+        let result = await (await this.userRepository.update(params.ID, {Password: hashPassword})).affected;
+        if (result !== 0)
+            return true;
+        else 
+            return false;
     }
 
 }
